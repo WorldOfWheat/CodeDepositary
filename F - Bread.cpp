7afe7804868b63594b7b161302dc41e2eb954cda
WorldOfWheat <86335529+WorldOfWheat@users.noreply.github.com>
2022-07-25 18:41:20 +0800

    Create F - Bread.cpp

---
+++ F - Bread.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define int long long
+
+using namespace std;
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    while (n--) {
+        int a;
+        cin >> a;
+        m -= a;
+        pq.push(a);
+    }
+    if (m > 0)
+        pq.push(m);
+    int cost = 0;
+    while (pq.size() > 1) {
+        int a = pq.top();
+        pq.pop();
+        int b = pq.top();
+        pq.pop();
+        pq.push(a + b);
+        cost += a + b;
+    }
+    cout << cost << endl;
+
+
+    return 0;
+}
