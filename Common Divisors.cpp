58fa6713bcecc1a87a872bfaa52d38e0ba5f7abd
WorldOfWheat <86335529+WorldOfWheat@users.noreply.github.com>
2022-07-25 18:50:47 +0800

    Create Common Divisors.cpp

---
+++ Common Divisors.cpp
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+#define int long long
+ 
+using namespace std;
+__attribute__((optimize("-O3")))
+ 
+int arr[1000000+1];
+ 
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+ 
+    int n;
+    cin >> n;
+    int maxi = 0;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        arr[a]++;
+        maxi = max(maxi, a);
+    }
+    int ans = 0;
+    for (int i = 1; i <= maxi; i++) {
+        int cnt = 0;
+        for (int j = i; j <= maxi; j+=i) {
+            cnt += arr[j];
+        }
+        if (cnt > 1) ans = max(ans, i);
+    }
+    cout << ans << endl;
+    return 0;
+}
