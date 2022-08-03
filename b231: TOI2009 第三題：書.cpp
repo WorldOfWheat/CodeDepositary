6321a9427a365f95092fec9abd2fd87ef8cbde09
WorldOfWheat <86335529+WorldOfWheat@users.noreply.github.com>
2022-08-04 01:03:28 +0800

    Update b231: TOI2009 第三題：書.cpp

--- b231: TOI2009 第三題：書.cpp
+++ b231: TOI2009 第三題：書.cpp
@@ -1,31 +1,48 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
+#define int long long
+#define pii pair<int, int>
+#define tp tuple<int, int, int>
+#define mp(x, y) make_pair(x, y)
+#define F first
+#define S second
 
 using namespace std;
+__attribute__((optimize("-O3")))
 
-priority_queue<pair<int, int>, vector<pair<int, int>>, less<pair<int, int>>> pq;
-int main() {
-    ios_base::sync_with_stdio(false);
+struct cmp {
+    bool operator()(pii x, pii y) {
+        return x.S < y.S;
+    }
+};
+
+int n;
+priority_queue<pii, vector<pii>, cmp> pq;
+
+void solve() {
+
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        pq.push( mp(a, b) );
+    }
+    int ans = 0;
+    int now = 0;
+    while (!pq.empty()) {
+        pii top = pq.top();
+        pq.pop();
+        now += top.F;
+        ans = max(ans, now + top.S);
+    }
+    cout << ans << "\n";
+
+    return;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int n;
-    while (cin >> n) {
-        for (int i = 0; i < n; i++) {
-            int a, b;
-            cin >> a >> b;
-            pq.push({b, a});
-        }
-        int print_time = 0;
-        int times = 0;
-        while (!pq.empty()) {
-            print_time += pq.top().second;
-            times = max(times, print_time + pq.top().first);
-            pq.pop();
-        }
-        cout << times << endl;
-    }
+    while (cin >> n) solve();
 
     return 0;
 }
