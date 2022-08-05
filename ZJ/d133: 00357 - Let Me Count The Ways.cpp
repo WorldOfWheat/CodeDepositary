6758a57b6bb3bd41aeece19425a378cb7a482c74
WorldOfWheat <86335529+WorldOfWheat@users.noreply.github.com>
2022-08-06 01:07:25 +0800

    Update d133: 00357 - Let Me Count The Ways.cpp

--- ZJ/d133: 00357 - Let Me Count The Ways.cpp
+++ ZJ/d133: 00357 - Let Me Count The Ways.cpp
@@ -1,25 +1,51 @@
-#include <iostream>
-#include <cstring>
+#include <bits/stdc++.h>
+#define int long long
+#define pii pair<int, int>
+#define tp tuple<int, int, int>
+#define mp(x, y) make_pair(x, y)
+#define F first
+#define S second
+#define MAX 30000
 
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+int n;
+vector<int> ve_dp(MAX + 1);
 
-    const int coins[5] = {1, 5, 10, 25, 50};
-    long long dp[30000+1] = {1};
-    for (auto &a : coins) {
-        for (int i = a; i <= 30000; i++) {
-            dp[i] += dp[i-a];
+void solve() {
+
+    ve_dp[0] = 1;
+    for (int i = 1; i <= MAX; i++) {
+        ve_dp[i] += ve_dp[i-1];
+    }
+    for (int i = 5; i <= MAX; i++) {
+        ve_dp[i] += ve_dp[i-5];
+    }
+    for (int i = 10; i <= MAX; i++) {
+        ve_dp[i] += ve_dp[i-10];
+    }
+    for (int i = 25; i <= MAX; i++) {
+        ve_dp[i] += ve_dp[i-25];
+    }
+    for (int i = 50; i <= MAX; i++) {
+        ve_dp[i] += ve_dp[i-50];
+    }
+    while (cin >> n) {
+        if (ve_dp[n] == 1) {
+            cout << "There is only 1 way to produce " << n << " cents change." << "\n";
+        } else {
+            cout << "There are " << ve_dp[n] << " ways to produce " << n << " cents change." << "\n";
         }
     }
-    int n;
-    while (cin >> n) {
-        if (dp[n] == 1) cout << "There is only 1 way to produce " << n << " cents change." << endl;
-        else cout << "There are " << dp[n] << " ways to produce " << n << " cents change." << endl;
-    }
+
+    return;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    solve();
 
     return 0;
 }
