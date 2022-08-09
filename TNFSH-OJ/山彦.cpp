804ef807c584ab0a5b79738d84a980155e51cb46
WorldOfWheat <86335529+WorldOfWheat@users.noreply.github.com>
2022-08-09 18:33:03 +0800

    Create 山彦.cpp

---
+++ TNFSH-OJ/山彦.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pii pair<int, int>
+#define tp tuple<int, int, int>
+#define mp(x, y) make_pair(x, y)
+#define F first
+#define S second
+
+using namespace std;
+
+void solve() {
+
+    int a, b, c;
+    cin >> a >> b;
+    cout << "Do you want to say " << a << " and " << b << " ??" << "\n";
+
+    return;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    solve();
+
+    return 0;
+}
