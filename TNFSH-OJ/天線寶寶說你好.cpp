3068cb6295f624f2e018f36eaa00d3a0b3e8de47
WorldOfWheat <86335529+WorldOfWheat@users.noreply.github.com>
2022-08-09 17:02:23 +0800

    Create 天線寶寶說你好.cpp

---
+++ TNFSH-OJ/天線寶寶說你好.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pii pair<int, int>
+#define tp tuple<int, int, int>
+#define mp(x, y) make_pair(x, y)
+#define F first
+#define S second
+
+using namespace std;
+
+void solve() {
+
+    string a, b, c;
+    cin >> a >> b >> c;
+    cout << "Hello, " << a << ", " << b << ", and " << c << '!' << "\n";
+
+    return;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    solve();
+
+    return 0;
+}
