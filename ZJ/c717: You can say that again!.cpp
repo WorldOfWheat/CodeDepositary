39eb736b740d07a4cedb1b26e6001f6a5e0f3ab7
WorldOfWheat <86335529+WorldOfWheat@users.noreply.github.com>
2022-08-15 22:19:36 +0800

    Create c717: You can say that again!.cpp

---
+++ ZJ/c717: You can say that again!.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pii pair<int, int>
+#define F first
+#define S second
+#define ln "\n"
+#define sp " "
+
+using namespace std;
+
+string str;
+
+void solve() {
+
+    getline(cin, str);
+    cout << str << sp << str << ln;
+
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    solve();
+
+    return 0;
+}
