713ff2739ca62c74a6620839a2728f3d9d70c938
WorldOfWheat <86335529+WorldOfWheat@users.noreply.github.com>
2022-08-23 20:44:36 +0800

    Update fastpow.cpp

--- 模板/fastpow.cpp
+++ 模板/fastpow.cpp
@@ -1,11 +1,13 @@
-int fastpow(int x, int y) {
+int fastpow(int x, int y, int mod) {
     int res = 1;
     while (y > 0) {
         if (y & 1) {
             res *= x;
+            res %= mod;
         }
         y >>= 1;
         x *= x;
+        x %= mod;
     }
     return res;
 }
